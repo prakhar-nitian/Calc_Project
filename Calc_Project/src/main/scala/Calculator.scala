0279f8e44473d96d9161ceda89322a36ca8582f2
Prakhar Gupta <53451374+prakhar-nitian@users.noreply.github.com>
2021-07-30 17:53:59 +0530

    ok
    ok

---
+++ Calc_Project/src/main/scala/Calculator.scala
@@ -0,0 +1,26 @@
+import scala.io.StdIn.readLine
+
+class Calculator {
+    println("Operations Available : \n 1. add \n 2. sub \n 3. multiply \n 4. divide")
+
+    def add(x: Int, y:Int): Int = { x + y  }
+    def sub(x: Int , y:Int): Int = { x - y }
+    def multiply(x: Int , y:Int): Int = { x * y  }
+    def divide(x: Int , y:Int): Int = { x / y }
+    println("Enter your First Number: ")
+    val firstNumber:Int = readLine().toInt
+    println("Enter your Second Number: ")
+    val secondNumber: Int= readLine().toInt
+    println("Enter operation do you want")
+    val operation: String = readLine()
+
+    /* println(add(2,3)) ; */
+
+    operation match {
+      case "add" => println(add(firstNumber:Int,secondNumber :Int))
+      case "sub" => println(sub(firstNumber:Int,secondNumber :Int))
+      case "multiply" => println(multiply(firstNumber:Int,secondNumber :Int))
+      case "divide" => println(divide(firstNumber:Int,secondNumber :Int))
+      case _ => "Incorrect Operator"
+    }
+}
---
+++ Calc_Project/src/test/scala/CalculatorTest.scala
@@ -0,0 +1,26 @@
+import org.scalatest.FunSuite
+
+class CalculatorTest extends FunSuite {
+
+  val calculator_obj = new Calculator
+
+
+  test("Add Test Cases") {
+    assert(calculator_obj.add(2, 0) == 2)
+    assert(calculator_obj.add(-2, -2) == -4)
+  }
+  test("Subtract Test Cases") {
+    assert(calculator_obj.sub(2, 0) == 2)
+    assert(calculator_obj.sub(-2, -2) == 0)
+  }
+
+  test("multiply Test Cases") {
+    assert(calculator_obj.multiply(2, 0) == 0)
+    assert(calculator_obj.multiply(0, 4) == 0)
+    assert(calculator_obj.multiply(0, 0) == 0)
+  }
+
+  test("Divide Test case") {
+    assertThrows[ArithmeticException](calculator_obj.divide(1, 0))
+  }
+}
