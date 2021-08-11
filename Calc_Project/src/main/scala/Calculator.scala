aec1b4ded78b2507893dc006632f15ca447f4d11
Prakhar Gupta <53451374+prakhar-nitian@users.noreply.github.com>
2021-08-11 11:36:45 +0530

    Update Calculator.scala

--- Calc_Project/src/main/scala/Calculator.scala
+++ Calc_Project/src/main/scala/Calculator.scala
@@ -7,20 +7,25 @@ class Calculator {
     def sub(x: Int , y:Int): Int = { x - y }
     def multiply(x: Int , y:Int): Int = { x * y  }
     def divide(x: Int , y:Int): Int = { x / y }
-    println("Enter your First Number: ")
-    val firstNumber:Int = readLine().toInt
-    println("Enter your Second Number: ")
-    val secondNumber: Int= readLine().toInt
-    println("Enter operation do you want")
-    val operation: String = readLine()
+    def perform_func: Unit ={
+      println("Enter your First Number: ")
+      val firstNumber:Int = readLine().toInt
+      println("Your first number is"+firstNumber)
+      println("Enter your Second Number: ")
+      val secondNumber: Int= readLine().toInt
+      println("Your second number is"+secondNumber)
+      println("Enter operation do you want")
+      val operation: String = readLine()
 
-    /* println(add(2,3)) ; */
+      /* println(add(2,3)) ; */
 
-    operation match {
-      case "add" => println(add(firstNumber:Int,secondNumber :Int))
-      case "sub" => println(sub(firstNumber:Int,secondNumber :Int))
-      case "multiply" => println(multiply(firstNumber:Int,secondNumber :Int))
-      case "divide" => println(divide(firstNumber:Int,secondNumber :Int))
-      case _ => "Incorrect Operator"
+      operation match {
+        case "add" => println(add(firstNumber:Int,secondNumber :Int))
+        case "sub" => println(sub(firstNumber:Int,secondNumber :Int))
+        case "multiply" => println(multiply(firstNumber:Int,secondNumber :Int))
+        case "divide" => println(divide(firstNumber:Int,secondNumber :Int))
+        case _ => println("Incorrect Operator")
+      }
     }
+
 }
